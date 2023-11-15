db2d6511a35ab7654ca7bf3cfbd4f67104c06f88
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2023-11-15 22:18:54 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,84 @@
+
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var level =0;
+var started = false;
+
+
+$(document).keydown(function(event){
+    
+
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started=true;
+    }
+    
+
+})
+
+$(".btn").click( function handler(){
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
+})
+
+function nextSequence(){
+    userClickedPattern=[];
+    level++;
+    $("#level-title").text("Level "+level);
+    var random = Math.floor(Math.random()*4);
+    randomChosenColor = buttonColors[random];
+    gamePattern.push(randomChosenColor);
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+    
+}
+
+function playSound(name){
+    var audio = new Audio ("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100);
+}
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        if(gamePattern.length===userClickedPattern.length){
+           setTimeout(() => {
+                nextSequence();
+           }, 1000);
+        }
+    }
+    else{
+        playSound("wrong")
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        startOver();
+    }
+}
+
+function startOver(){
+    gamePattern = [];
+    level = 0;
+    started = false;
+    $("#level-title").text("Press A Key to Start");
+}
+
+
+
+
+
